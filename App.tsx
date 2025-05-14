525cca769f43107f5a8bddb2d4125a08dc598b3e
Felipe Dantas <dantas.contatod@gmail.com>
2025-05-14 01:43:31 -0300

    feat: text-preset

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Text } from './src/components/Text/Text';
+import {SafeAreaView} from 'react-native';
+import {Text} from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
-
-
   return (
     <SafeAreaView>
-      <Text>EAE</Text>
+      <Text>Testando</Text>
     </SafeAreaView>
   );
 }
-
 
 export default App;
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,6 +1,47 @@
 import React from 'react';
-import {Text as RNText, TextProps} from 'react-native';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  type TextStyle,
+} from 'react-native';
 
-export const Text = (props: TextProps) => {
-  return <RNText>{props.children}</RNText>;
+interface TextProps extends RNTextProps {
+  preset?: TextVariants;
+  children: React.ReactNode;
+}
+
+export const Text = ({
+  children,
+  preset = 'headingMedium',
+  style,
+  ...rest
+}: TextProps) => {
+  return (
+    <RNText style={[$fontSizes[preset], style]} {...rest}>
+      {children}
+    </RNText>
+  );
+};
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
 };
